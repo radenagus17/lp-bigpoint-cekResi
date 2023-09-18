baa515784a8a7e31efb937b52060b6a2874680ea
radenagus17_ <vaghuz@gmail.com>
2023-09-18 10:28:49 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+interface AppProps {}
+
+const App: FC<AppProps> = ({}) => {
+  return <div className="font-bold text-3xl">Hallo world</div>;
+};
+
+export default App;
